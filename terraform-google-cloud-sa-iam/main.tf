1610a769ec02450878fe4eac60dd63c523d66dfb
ayushshukla <ayush25shukla@gmail.com>
2023-03-13 10:00:27 +0000

    Push-13March-2023-03:30PM

---
+++ terraform-google-cloud-sa-iam/main.tf
@@ -0,0 +1,9 @@
+/******************************************
+  Google Cloud Project IAM Policy
+ *****************************************/
+resource "google_service_account_iam_binding" "sa_iam" {
+  service_account_id  = var.service_account
+  count   = length(var.roles)
+  role    = var.roles[count.index]
+  members = var.members
+}
