6a65675ba72f37a730dfecf52a56d1ae5d0bb93f
ayushshukla <ayush25shukla@gmail.com>
2023-03-13 06:38:12 +0000

    Push-13-March-2023-12:08PM

--- terraform-google-cloud-apis/main.tf
+++ terraform-google-cloud-apis/main.tf
@@ -1,7 +1,6 @@
 resource "google_project_service" "gcp_apis" {
   for_each                   = toset(var.gcp_apis_list)
-  count                      = length(var.project_id)
-  project                    = var.project_id[count.index]
+  project                    = [for p in var.project_id: p]
   service                    = each.key
   disable_on_destroy         = var.disable_apis_on_destroy
   disable_dependent_services = var.disable_dependent_apis
