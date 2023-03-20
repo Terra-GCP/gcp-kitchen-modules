0219874404260cdcc8a7d211177dd712930cfec0
ayushshukla <ayush25shukla@gmail.com>
2023-03-20 12:35:31 +0000

    Push-20-03-2023-06:05PM-SA-KEY

--- terraform-google-cloud-sa-key/outputs.tf
+++ terraform-google-cloud-sa-key/outputs.tf
@@ -1,7 +1,3 @@
-output "service_account_details" {
-    value = google_service_account.service_account
-}
-
 output "service_account_key_details" {
     value = google_service_account_key.service_acccount_key
     sensitive = true
