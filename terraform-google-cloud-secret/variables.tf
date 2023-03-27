5370926b782642b4c951fe1f36b1666f63b5a5eb
ayushshukla <ayush25shukla@gmail.com>
2023-03-27 09:53:10 +0000

    Push-27-March-2023-03:23PM

---
+++ terraform-google-cloud-secret/variables.tf
@@ -0,0 +1,13 @@
+variable "project_id" {
+  type        = string
+}
+variable "labels " {
+  type = map
+}
+variable "auto_replication" {
+  type = bool
+}
+variable "secret_data" {
+  type = any
+}
+
