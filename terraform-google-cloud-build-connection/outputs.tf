770efa65bc5638fddc5f6174a901ba11dc118537
ayushshukla <ayush25shukla@gmail.com>
2023-03-28 08:32:18 +0000

    Push-28-March-2023-02:02PM

--- terraform-google-cloud-build-connection/outputs.tf
+++ terraform-google-cloud-build-connection/outputs.tf
@@ -1,3 +1,6 @@
-output "build_trigger_details" {
-    value = google_cloudbuild_trigger.build-trigger
+output "build_connection_details" {
+    value = google_cloudbuildv2_connection.build_connection
+}
+output "repository_connection_details" {
+    value = google_cloudbuildv2_repository.repository_connection
 }
